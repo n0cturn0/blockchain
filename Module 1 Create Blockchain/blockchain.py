6a19c440359bd88ff51a83225434651127a416d6
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-21 13:53:54 -0400

    Função que gera o hash Genesis

--- Module 1 Create Blockchain/blockchain.py
+++ Module 1 Create Blockchain/blockchain.py
@@ -23,3 +23,19 @@ class Blockchain:
     #Método para retornar o bloco anterior
     def get_previsous_block(self):
         return self.chain[-1]
+    
+    #Dificuldade da tarefa Quanto mais zero a esquerda mais difícil Gerando o hash
+    def proof_of_work(self, previous_proof):
+        new_proof =1
+        check_proof =  False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest() #Gerando hash
+            if hash_operation[:4] ==  '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+                return new_proof
+                
+        
+        
+        
