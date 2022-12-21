b29110f55ca6b1ca3331adbacfd37f750e4093a4
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-21 12:53:18 -0400

    Correção de identação

--- Module 1 Create Blockchain/blockchain.py
+++ Module 1 Create Blockchain/blockchain.py
@@ -7,6 +7,15 @@ from flask import Flask, jsonify
 
 class Blockchain:
     def __init__(self): #Inicializa a classe
-    self.chain = []
-    self.create_block(proof = 1, previous_hash='0')
+        self.chain=[]
+        self.create_block(proof = 1, previous_hash='0')
     
+    #criando dicionário do bloco
+    def create_block(self, proof,previous_hash): #Método
+        block ={'index': len(self.chain)+1,
+                'timestamp':str(datetime.datetime.now()),
+                'proof': proof,
+                'previous_hash': previous_hash}
+        #Incluindo o bloco a cadeia a blockchain
+        self.chain.append(block) # append adiciona blck ao chain []
+        return block
