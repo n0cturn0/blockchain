2b51491818b7e91d52334aaa4afc55cd5289e665
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-12-21 12:27:34 -0400

    Iniciando a classe principal geradora do blockchain

--- Module 1 Create Blockchain/blockchain.py
+++ Module 1 Create Blockchain/blockchain.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
 
-This is a temporary script file.
-"""
+#Parte 1, criar um blockchain
 
+class Blockchain:
+    def __init__(self): #Inicializa a classe
+    self.chain = []
+    self.create_block(proof = 1, previous_hash='0')
+    
