0eb4110fd30f5a81bba6b8c0b4f8f9ead415234e
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-01-02 20:42:49 -0400

    Comment details

--- Module 1 Create Blockchain/blockchain.py
+++ Module 1 Create Blockchain/blockchain.py
@@ -39,7 +39,7 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-        
+    #Comment
                 
         
         
